25c4effaa1ee593903d41889e39cbd2f05e1bf2c
Jonas Goseberg <j.goseberg@protonmail.com>
2025-05-26 13:35:59 +0200

    added RecipeController

---
+++ CookBook_Api.Tests/Cotrollers/RecipeControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using CookBook_Api.Controllers;
+using CookBook_Api.Interfaces;
+using CookBook_Api.DTOs;
+using AutoMapper;
+using CookBook_Api.Mappings;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using CookBook_Api.Models;
+
+namespace CookBook_Api.Tests.Cotrollers
+{
+    [TestFixture]
+    public class RecipeControllerTests
+    {
+        private Mock<IRecipeRepository> _recipeRepositoryMock;
+        private IMapper _mapper;
+
+        private RecipeController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _recipeRepositoryMock = new Mock<IRecipeRepository>();
+            _mapper = MapperConfig.InitializeAutoMapper();
+            _controller = new RecipeController(_recipeRepositoryMock.Object, _mapper);
+        }
+
+        [Test]
+        public async Task GetAllRecipesShouldReturnOk()
+        {
+            var recipes = new List<RecipeDTO>
+            {
+                new() { Name = "Foo"},
+                new() { Name = "Bar"}
+            };
+
+            _recipeRepositoryMock.Setup(r => r.GetAllRecipesAsync())
+                .ReturnsAsync(recipes);
+
+            var result = await _controller.GetAllRecipes();            
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+
+                var recipesFromController = (OkObjectResult?)result.Result;
+                var returnedRecipes = recipesFromController?.Value as IEnumerable<Recipe>;
+
+                Assert.That(returnedRecipes?.Count(), Is.EqualTo(2));
+            });
+        }
+    }
+}
---
+++ CookBook_Api/Controllers/RecipeController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CookBook_Api.DTOs;
+using CookBook_Api.Interfaces;
+using CookBook_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookBook_Api.Controllers
+{
+    [ApiController]
+    [Route("/api/")]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly IMapper _mapper;
+
+        public RecipeController(IRecipeRepository recipeRepository, IMapper mapper)
+        {
+            _recipeRepository = recipeRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetAllRecipes()
+        {
+            var recipes = await _recipeRepository.GetAllRecipesAsync();
+
+            var test = _mapper.Map<IEnumerable<Recipe>>(recipes);
+            return Ok(test);
+            
+        }
+    }
+}
