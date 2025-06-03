639aa0464c4031a4be476d2cb0166d9e32fb9bbf
Jonas Goseberg <j.goseberg@protonmail.com>
2025-06-03 14:53:55 +0200

    added Result<T> Pattern

---
+++ CookBook_Api/Common/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace CookBook_Api.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public string? ErrorMessage { get; }
+        public T? Value { get; }
+        
+        protected Result(bool isSuccess, T? value, string? errorMessage)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+            Value = value;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, errorMessage: null);
+        public static Result<T> Fail(string error) => new(false, value: default, error);
+    }
+}
